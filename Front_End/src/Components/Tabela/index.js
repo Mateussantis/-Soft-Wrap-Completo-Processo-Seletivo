6394fa1edfac02781f6b7ade88b32359dda9cdf2
Mateussantis <mateussantiss@gmail.com>
2020-09-27 18:52:10 -0300

    Bug git / Componentes Sepadados

--- Front_End/src/Components/Tabela/index.js
+++ Front_End/src/Components/Tabela/index.js
@@ -1,105 +1,105 @@
-// import React , {Component} from 'react';
-// import Modal from 'react-bootstrap/Modal';
-// import { Button } from 'react-bootstrap';
-// import ReactPaginate from 'react-paginate'; 
+import React , {Component} from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { Button } from 'react-bootstrap';
+import ReactPaginate from 'react-paginate'; 
 
-// export default class Tabela extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       // lista: []
-//     }
-//   }
+export default class Tabela extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // lista: []
+    }
+  }
 
-//   render() {  
-//   return (
-//   <div>
+  render() {  
+  return (
+  <div>
 
-//     <h3>Tabela De Usuarios</h3>
+    <h3>Tabela De Usuarios</h3>
 
-//     <table>
-//     <tr>
-//       <td className="nomes"><strong>Identificação</strong></td>
-//       <td className="nomes"><strong>Nome</strong></td>
-//       <td className="nomes"><strong>Idade</strong></td>
-//       <td className="nomes"><strong>Estado Civil</strong></td>
-//       <td className="nomes"><strong>Cpf</strong></td>
-//       <td className="nomes"><strong>Cidade</strong></td>
-//       <td className="nomes"><strong>Estado</strong></td>
-//       <td className="nomes"><strong>Função 1</strong></td>
-//       <td className="nomes"><strong>Função 2</strong></td>
-//     </tr>
-//     {
-//       this.props.lista.map(function(user, i){
-//         return(
-//           <tr key={user.id}>
-//             <td>{user.id}</td>
-//             <td>{user.nome}</td>
-//             <td>{user.idade}</td>
-//             <td>{user.estado_civil}</td>
-//             <td>{user.cpf}</td>
-//             <td>{user.cidade}</td>
-//             <td>{user.estado}</td>
-//             <td><button className="buttonAction" onClick={() => this.props.abrirModalAlterar(user)}>Alterar</button></td>
-//             <td><button className="buttonAction" onClick={e => this.props.deletar(user.id)} >Deletar</button></td>
-//           </tr>
-//         );
-//       }.bind(this))
-//     }
-//     </table>
+    <table>
+    <tr>
+      <td className="nomes"><strong>Identificação</strong></td>
+      <td className="nomes"><strong>Nome</strong></td>
+      <td className="nomes"><strong>Idade</strong></td>
+      <td className="nomes"><strong>Estado Civil</strong></td>
+      <td className="nomes"><strong>Cpf</strong></td>
+      <td className="nomes"><strong>Cidade</strong></td>
+      <td className="nomes"><strong>Estado</strong></td>
+      <td className="nomes"><strong>Função 1</strong></td>
+      <td className="nomes"><strong>Função 2</strong></td>
+    </tr>
+    {
+      this.props.lista.map(function(user, i){
+        return(
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.nome}</td>
+            <td>{user.idade}</td>
+            <td>{user.estado_civil}</td>
+            <td>{user.cpf}</td>
+            <td>{user.cidade}</td>
+            <td>{user.estado}</td>
+            <td><button className="buttonAction" onClick={() => this.props.abrirModalAlterar(user)}>Alterar</button></td>
+            <td><button className="buttonAction" onClick={e => this.props.deletar(user.id)} >Deletar</button></td>
+          </tr>
+        );
+      }.bind(this))
+    }
+    </table>
 
-//     <ReactPaginate
-//       previousLabel={"<"}
-//       nextLabel={">"}
-//       breakLabel={"..."}
-//       breakClassName={"break-me"}
-//       pageCount={this.props.pageCount}
-//       marginPagesDisplayed={2}
-//       pageRangeDisplayed={5}
-//       onPageChange={this.props.handlePageClick.bind(this)}
-//       containerClassName={"pagination"}
-//       subContainerClassName={"pages pagination"}
-//       activeClassName={"active"}
-//     />
+    <ReactPaginate
+      previousLabel={"<"}
+      nextLabel={">"}
+      breakLabel={"..."}
+      breakClassName={"break-me"}
+      pageCount={this.props.pageCount}
+      marginPagesDisplayed={2}
+      pageRangeDisplayed={5}
+      onPageChange={this.props.handlePageClick.bind(this)}
+      containerClassName={"pagination"}
+      subContainerClassName={"pages pagination"}
+      activeClassName={"active"}
+    />
         
-//     <Modal show={this.props.modalAltera} onHide={this.props.toggleAltera}>
-//       <form onSubmit={this.props.alterar}>
-//         <div className="titulo">
-//           <h4 className="h4">Alterar</h4>
-//         </div>
-//         <Modal.Body className="corpo">
-//         <div className="corpo_form">
-//           <div className="label">Nome
-//           <input text="a" name="nome" value={this.props.altera.nome} onChange={this.props.atualizarDadosAlterar.bind(this)} />
-//           </div>
-//         <div className="label">Idade
-//           <input label="Idade" name="idade" type="number"value={this.props.altera.idade} onChange={this.props.atualizarDadosAlterar.bind(this)} />
-//         </div>
-//         <div className="label">Estado Civil
-//           <input name="estado_civil" value={this.props.altera.estado_civil} onChange={this.props.atualizarDadosAlterar.bind(this)} />
-//         </div>
-//         <div className="label">CPF
-//           <input name="cpf" value={this.props.altera.cpf} onChange={this.props.atualizarDadosAlterar.bind(this)} />
-//         </div>
-//         <div className="label">Cidade
-//           <input name="cidade" value={this.props.altera.cidade} onChange={this.props.atualizarDadosAlterar.bind(this)} />
-//         </div>
-//         <div className="label">Estado
-//           <input name="estado" value={this.props.altera.estado} onChange={this.props.atualizarDadosAlterar.bind(this)} />
-//         </div>
-//         </div>
-//         </Modal.Body>
-//         <Modal.Footer className="footer">
-//           <Button className="btnFooter" variant="secondary" onClick={this.props.toggleFecha}>Close</Button>
-//           <Button className="btnFooter" variant="primary" type="submit" onClick={this.props.toggleFecha}>Salvar</Button>
-//         </Modal.Footer>
-//       </form>
-//     </Modal>
+    <Modal show={this.props.modalAltera} onHide={this.props.toggleAltera}>
+      <form onSubmit={this.props.alterar}>
+        <div className="titulo">
+          <h4 className="h4">Alterar</h4>
+        </div>
+        <Modal.Body className="corpo">
+        <div className="corpo_form">
+          <div className="label">Nome
+          <input text="a" name="nome" value={this.props.altera.nome} onChange={this.props.atualizarDadosAlterar.bind(this)} />
+          </div>
+        <div className="label">Idade
+          <input label="Idade" name="idade" type="number"value={this.props.altera.idade} onChange={this.props.atualizarDadosAlterar.bind(this)} />
+        </div>
+        <div className="label">Estado Civil
+          <input name="estado_civil" value={this.props.altera.estado_civil} onChange={this.props.atualizarDadosAlterar.bind(this)} />
+        </div>
+        <div className="label">CPF
+          <input name="cpf" value={this.props.altera.cpf} onChange={this.props.atualizarDadosAlterar.bind(this)} />
+        </div>
+        <div className="label">Cidade
+          <input name="cidade" value={this.props.altera.cidade} onChange={this.props.atualizarDadosAlterar.bind(this)} />
+        </div>
+        <div className="label">Estado
+          <input name="estado" value={this.props.altera.estado} onChange={this.props.atualizarDadosAlterar.bind(this)} />
+        </div>
+        </div>
+        </Modal.Body>
+        <Modal.Footer className="footer">
+          <Button className="btnFooter" variant="secondary" onClick={this.props.toggleFecha}>Close</Button>
+          <Button className="btnFooter" variant="primary" type="submit" onClick={this.props.toggleFecha}>Salvar</Button>
+        </Modal.Footer>
+      </form>
+    </Modal>
     
-//   </div>
+  </div>
 
-//   )
-// }
-// }
+  )
+}
+}
 
 
--- Front_End/src/pages/Home/App.js
+++ Front_End/src/pages/Home/App.js
@@ -1,206 +1,206 @@
-// import React, { Component} from 'react';
-// import './style/App.css';
-// import { api } from '../../services/api';
-// import { Button } from 'react-bootstrap';
-// import Modal from 'react-bootstrap/Modal';
-// import ReactPaginate from 'react-paginate'; 
-// import Cadastro from '../../Components/Cadastro';
-// import Tabela from '../../Components/Tabela';
+import React, { Component} from 'react';
+import './style/App.css';
+import { api } from '../../services/api';
+import { Button } from 'react-bootstrap';
+import Modal from 'react-bootstrap/Modal';
+import ReactPaginate from 'react-paginate'; 
+import Cadastro from '../../Components/Cadastro';
+import Tabela from '../../Components/Tabela';
 
-// export default class App extends Component {
-//   constructor(props) {
-//     super(props);    
-//     this.state = {
-//       lista: [],
-//       cadastro: {
-//         nome: "",
-//         idade: "",
-//         estado_civil: "",
-//         cpf: "",
-//         cidade: "",
-//         estado: ""
-//       },
-//       altera: {
-//         nome: "",
-//         idade: "",
-//         estado_civil: "",
-//         cpf: "",
-//         cidade: "",
-//         estado: ""
-//       },
-//       modalAltera: false,
+export default class App extends Component {
+  constructor(props) {
+    super(props);    
+    this.state = {
+      lista: [],
+      cadastro: {
+        nome: "",
+        idade: "",
+        estado_civil: "",
+        cpf: "",
+        cidade: "",
+        estado: ""
+      },
+      altera: {
+        nome: "",
+        idade: "",
+        estado_civil: "",
+        cpf: "",
+        cidade: "",
+        estado: ""
+      },
+      modalAltera: false,
 
-//       offSet: 0,
-//       orgtableData: [],
-//       perPage: 3,
-//       currentPage: 0,
+      offSet: 0,
+      orgtableData: [],
+      perPage: 3,
+      currentPage: 0,
 
-//     }
-//     this.cadastrar = this.cadastrar.bind(this);
-//     this.deletar = this.deletar.bind(this);
-//     this.alterar = this.alterar.bind(this);
-//     this.handlePageClick = this.handlePageClick.bind(this);
-//   }
+    }
+    this.cadastrar = this.cadastrar.bind(this);
+    this.deletar = this.deletar.bind(this);
+    this.alterar = this.alterar.bind(this);
+    this.handlePageClick = this.handlePageClick.bind(this);
+  }
   
 
-//   listar = () => {
-//     api.get("users")
-//       .then(res => {
-//         const data = res.data
-//         const slice = data.slice(this.state.offSet, this.state.offSet + this.state.perPage)
+  listar = () => {
+    api.get("users")
+      .then(res => {
+        const data = res.data
+        const slice = data.slice(this.state.offSet, this.state.offSet + this.state.perPage)
 
-//         this.setState({ 
-//           pageCount: Math.ceil(data.length / this.state.perPage),
-//           orgtableData: res.data,
-//           lista: slice
-//         })
-//       }
-//     )
-//   }
+        this.setState({ 
+          pageCount: Math.ceil(data.length / this.state.perPage),
+          orgtableData: res.data,
+          lista: slice
+        })
+      }
+    )
+  }
 
-//   handlePageClick = (e) => {
-//     const selectedPage = e.selected;
-//     const offset = selectedPage * this.state.perPage;
+  handlePageClick = (e) => {
+    const selectedPage = e.selected;
+    const offset = selectedPage * this.state.perPage;
 
-//     this.setState({
-//       currentPage: selectedPage,
-//         offset: offset
-//     }, () => {
-//       this.loadMoreData()
-//     });
-//   };
+    this.setState({
+      currentPage: selectedPage,
+        offset: offset
+    }, () => {
+      this.loadMoreData()
+    });
+  };
 
-//   loadMoreData() {
-// 		const data = this.state.orgtableData;
+  loadMoreData() {
+		const data = this.state.orgtableData;
 		
-// 		const slice = data.slice(this.state.offset, this.state.offset + this.state.perPage)
-// 		this.setState({
-// 			pageCount: Math.ceil(data.length / this.state.perPage),
-// 			lista: slice
-// 		})
-//   }
+		const slice = data.slice(this.state.offset, this.state.offset + this.state.perPage)
+		this.setState({
+			pageCount: Math.ceil(data.length / this.state.perPage),
+			lista: slice
+		})
+  }
   
 
-//   cadastrar = (event) => {
-//     // event.preventDefault();
+  cadastrar = (event) => {
+    // event.preventDefault();
 
-//     api.post("users/", this.state.cadastro)
-//     .then(setTimeout(() => {
-//       this.listar();
-//     }, 1500));
+    api.post("users/", this.state.cadastro)
+    .then(setTimeout(() => {
+      this.listar();
+    }, 1500));
     
-//   }
+  }
 
-//   atualizarDados = (input) => {
-//     this.setState({
-//       cadastro: {
-//         ...this.state.cadastro,
-//         [input.target.name] : input.target.value
-//       } 
-//     });
-//   }
+  atualizarDados = (input) => {
+    this.setState({
+      cadastro: {
+        ...this.state.cadastro,
+        [input.target.name] : input.target.value
+      } 
+    });
+  }
 
-//   deletar = (id) => {
-//     api.delete("users/"+id)
-//     .then(res => {
-//       console.log("res:"+res);
-//       console.log("data"+res.data);
-//       }
-//     )
-//     .then(() => setTimeout(() => {
-//       this.listar();
-//     }, 1000))
-//   }
+  deletar = (id) => {
+    api.delete("users/"+id)
+    .then(res => {
+      console.log("res:"+res);
+      console.log("data"+res.data);
+      }
+    )
+    .then(() => setTimeout(() => {
+      this.listar();
+    }, 1000))
+  }
 
-//   alterar = (event) => {
-//     // event.preventDefault();
+  alterar = (event) => {
+    // event.preventDefault();
 
-//     let id = this.state.altera.id;
-//     let usuario = this.state.altera;
+    let id = this.state.altera.id;
+    let usuario = this.state.altera;
 
-//     this.setState({
-//       altera: {
-//         nome: this.state.altera.nome,
-//         idade: this.state.altera.idade,
-//         estado_civil: this.state.altera.estado_civil,
-//         cpf: this.state.altera.cpf,
-//         cidade: this.state.altera.cidade,
-//         estado: this.state.altera.estado
-//       }
-//     });
+    this.setState({
+      altera: {
+        nome: this.state.altera.nome,
+        idade: this.state.altera.idade,
+        estado_civil: this.state.altera.estado_civil,
+        cpf: this.state.altera.cpf,
+        cidade: this.state.altera.cidade,
+        estado: this.state.altera.estado
+      }
+    });
 
-//     api.put("users/"+id, usuario)
-//     .then(response => {
-//       console.log("id: "+id);
-//       console.log(response);
-//       console.log(response.data);
-//     })
-//     .then(setTimeout(() => {
-//       this.listar();
-//     }, 1000));
-//   }
+    api.put("users/"+id, usuario)
+    .then(response => {
+      console.log("id: "+id);
+      console.log(response);
+      console.log(response.data);
+    })
+    .then(setTimeout(() => {
+      this.listar();
+    }, 1000));
+  }
 
-//   atualizarDadosAlterar = (input) => {
-//     this.setState({
-//       altera: {
-//         ...this.state.altera,
-//         [input.target.name]: input.target.value
-//       }
-//     });
-//   }
+  atualizarDadosAlterar = (input) => {
+    this.setState({
+      altera: {
+        ...this.state.altera,
+        [input.target.name]: input.target.value
+      }
+    });
+  }
 
-//   toggleAltera = () => {
-//     this.setState({
-//       modalAltera: true
-//     });
-//   }
+  toggleAltera = () => {
+    this.setState({
+      modalAltera: true
+    });
+  }
 
-//   toggleFecha = () => {
-//     this.setState({
-//       modalAltera: false
-//     });
-//   }
+  toggleFecha = () => {
+    this.setState({
+      modalAltera: false
+    });
+  }
 
-//   abrirModalAlterar = (user) => {
-//     this.toggleAltera();
-//     this.setState({ altera: user});
-//   }
+  abrirModalAlterar = (user) => {
+    this.toggleAltera();
+    this.setState({ altera: user});
+  }
 
-//   UNSAFE_componentWillMount(){
-//     this.listar();
-//   }
+  UNSAFE_componentWillMount(){
+    this.listar();
+  }
 
-//   UNSAFE_componentDidMount(){
-//     console.log('Did');
-//   }
+  UNSAFE_componentDidMount(){
+    console.log('Did');
+  }
 
-//   UNSAFE_componentDidUpdate(){
-//     console.log("Update");
-//   }
+  UNSAFE_componentDidUpdate(){
+    console.log("Update");
+  }
 
-//   UNSAFE_componentWillUnmount(){
-//     console.log("Unmount");
-//   }
+  UNSAFE_componentWillUnmount(){
+    console.log("Unmount");
+  }
 
-//   render() {
-//     return(
-//       <div className="body">
+  render() {
+    return(
+      <div className="body">
 
-//         <Cadastro cadastro = {this.state.cadastro} cadastrar = {this.cadastrar} atualizarDados = {this.atualizarDados} />
+        <Cadastro cadastro = {this.state.cadastro} cadastrar = {this.cadastrar} atualizarDados = {this.atualizarDados} />
 
-//         <Tabela 
-//           listar={this.listar} lista={this.state.lista}
-//           modalAltera={this.state.modalAltera} altera={this.state.altera}
-//           toggleAltera={this.toggleAltera} abrirModalAlterar={this.abrirModalAlterar} alterar={this.alterar}  
-//           atualizarDadosAlterar={this.atualizarDadosAlterar} toggleFecha={this.toggleFecha}
-//           deletar={this.deletar} 
-//           orgtableData={this.state.orgtableData} perPage={this.state.perPage} offset={this.state.offset} 
-//           currentPage={this.state.currentPage} pageCount={this.state.pageCount} 
-//           handlePageClick={this.handlePageClick} loadMoreData={this.loadMoreData}  
-//         />
+        <Tabela 
+          listar={this.listar} lista={this.state.lista}
+          modalAltera={this.state.modalAltera} altera={this.state.altera}
+          toggleAltera={this.toggleAltera} abrirModalAlterar={this.abrirModalAlterar} alterar={this.alterar}  
+          atualizarDadosAlterar={this.atualizarDadosAlterar} toggleFecha={this.toggleFecha}
+          deletar={this.deletar} 
+          orgtableData={this.state.orgtableData} perPage={this.state.perPage} offset={this.state.offset} 
+          currentPage={this.state.currentPage} pageCount={this.state.pageCount} 
+          handlePageClick={this.handlePageClick} loadMoreData={this.loadMoreData}  
+        />
                  
-//       </div>
-//     );
-//   }
+      </div>
+    );
+  }
 
-// }
+}
